4158eb447521dbf8dce4d7f65541a4aaf211d529
Bram <bram@brams.dev>
2021-12-09 18:34:42 +0100

    Fix context value

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import './../styles/globals.scss';
 
-export const GlobalsContext = React.createContext({
-	positions: [
-		'TOP_LEFT',
-		'TOP_CENTER',
-		'TOP_RIGHT',
-		'MIDDLE_LEFT',
-		'MIDDLE_CENTER',
-		'MIDDLE_RIGHT',
-		'BOTTOM_LEFT',
-		'BOTTOM_CENTER',
-		'BOTTOM_RIGHT'
-	]
-});
+export const GlobalsContext = React.createContext();
 
 function MyApp({ Component, pageProps }) {
 	return (
-		<GlobalsContext.Provider>
+		<GlobalsContext.Provider
+			value={{
+				positions: [
+					'TOP_LEFT',
+					'TOP_CENTER',
+					'TOP_RIGHT',
+					'MIDDLE_LEFT',
+					'MIDDLE_CENTER',
+					'MIDDLE_RIGHT',
+					'BOTTOM_LEFT',
+					'BOTTOM_CENTER',
+					'BOTTOM_RIGHT'
+				]
+			}}
+		>
 			<Component {...pageProps} />
 		</GlobalsContext.Provider>
 	);
