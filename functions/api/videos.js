206154aa039a31fe81722c4124545995461f3158
Bram <bram@brams.dev>
2021-12-14 11:59:47 +0100

    Fix function

--- functions/api/videos.js
+++ functions/api/videos.js
@@ -1,14 +1,4 @@
-export async function onRequest(context) {
-	// Contents of context object
-	const {
-		request, // same as existing Worker API
-		env, // same as existing Worker API
-		params, // if filename includes [id] or [[path]]
-		waitUntil, // same as ctx.waitUntil in existing Worker API
-		next, // used for middleware or to fetch assets
-		data, // arbitrary space for passing data between middlewares
-	} = context;
-
+export async function onRequest() {
 	const result = await fetch('https://api.baserow.io/api/database/rows/table/39830/?user_field_names=true', {
 		headers: {
 			'Authorization': 'Token kr310bJ19V2MF19P3fMiyo0lxhM9I7dh'
